3f6812631f0cd1d8662809390345d359c9564c1c
Lohith Gowda M <lohigowda@Lohith-Gowdas-Macbook-Pro.local>
2023-10-09 22:50:17 +0530

    test

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -301,7 +301,6 @@ output "public_subnet" {
   description = "The ID of the Public subnet"
   value       = aws_subnet.web_subnet.id
 }
-
 output "public_subnet2" {
   description = "The ID of the Public subnet"
   value       = aws_subnet.web_subnet2.id
